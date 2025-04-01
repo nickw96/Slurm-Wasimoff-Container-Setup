573cdceb9c3b1d63c69eb1aed8cd81b21de96111
nickw96 <nick.weiler@st.ovgu.de>
2025-04-01 17:57:19 +0200

    Update services and clean slurm scripts

--- slurm-resources/epilog.sh
+++ slurm-resources/epilog.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-# nohup /bin/deno run --allow-env --allow-read= --allow-net /bin/wasimoff_provider/denoprovider/main.ts --workers 2 --url http://controller:4080 &
-# pid2disown=$(ps aux | grep deno | grep -v grep | awk 'print $2')
-# disown $pid2disown
 systemctl enable --now wasimoff_provider.service
--- slurm-resources/prolog.sh
+++ slurm-resources/prolog.sh
@@ -1,5 +1,2 @@
-#!/bin/bash
-# based on https://stackoverflow.com/questions/17440585/how-to-get-pid-of-process-by-specifying-process-name-and-store-it-in-a-variable
-# ps axf | grep deno | grep -v grep | awk '{print "kill " $1}' | sh
 systemctl stop wasimoff_provider.service
 systemctl disable wasimoff_provider.service
