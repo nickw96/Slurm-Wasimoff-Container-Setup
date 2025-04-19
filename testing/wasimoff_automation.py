d0bf5d16d922380d4b5dca5ce4621e9b0e6735f2
nickw96 <nick.weiler@st.ovgu.de>
2025-04-19 19:52:48 +0200

    Fix wasimoff automation and make broker service more fault tolerant

--- testing/wasimoff_automation.py
+++ testing/wasimoff_automation.py
@@ -11,29 +11,34 @@ def main():
 
     random.seed(42)
     call_list = []
+    new_list = []
     running_calls = 0
     i = 0
     towns = 0
     dt = 0
     endtime = 0
     dts = [0.5, 1.0, 2.0, 4.0]
-    endtimes = [500, 1000, 2000, 5000, 10000]
+    endtimes = [500, 1000, 2000, 5000]
     while True:
         while running_calls < 50:
             if random.randint(1,2) == 1:
                 # tsp
-                towns = random.randint(10,13)
+                towns = random.randint(9,12)
                 call_list.append(sb.Popen(["./client", "-exec", "tsp.wasm", "rand", f"{towns}"]))
             else:
                 # proxels
                 dt = random.randint(0,3)
-                endtime = random.randint(0,4)
+                endtime = random.randint(0,3)
                 call_list.append(sb.Popen(["./client", "-exec", "proxels.wasm", "-dt", f"{dt}", "-endtime", f"{endtime}"]))
             running_calls = running_calls + 1
         for call in call_list:
             if call.poll() != None:
                 running_calls = running_calls - 1
-        time.sleep(random.randint(1,5))
+            else:
+                new_list.append(call)
+        call_list = [call for call in call_list if call in new_list]
+        new_list = []
+        time.sleep(random.randint(1,10))
 
 if __name__ == "__main__":
     main()
