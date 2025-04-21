65b2c6b2f3c2517d47643b84348268e04ec54706
nickw96 <nick.weiler@st.ovgu.de>
2025-04-21 18:37:06 +0200

    Fix extract_log.sh

--- testing/extract_log.sh
+++ testing/extract_log.sh
@@ -2,4 +2,4 @@
 # needs to be executed with 'sudo'
 START=$(sed -e '1!d' $1)
 STOP=$(sed -e '2!d' $1)
-journalctl -u $2 --since $START --until $STOP >> broker_$1
+journalctl -u $2 --since $START --until $STOP >> $1-$2.log
