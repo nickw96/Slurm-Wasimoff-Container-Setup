9dd8502610593042966af56719853b4cf0b244f9
nickw96 <nick.weiler@st.ovgu.de>
2025-04-28 21:08:05 +0200

    Add more slurm jobs

--- jobs/job_5.sh
+++ jobs/job_5.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # srun -N1 -l -D ./Slurm-Wasimoff-Container-Setup/CloverLeaf_Serial cp InputDecks/clover.in clover.in
-srun -l -D Slurm-Wasimoff-Container-Setup/CloverLeaf_Serial cp InputDecks/clover_bm2.in clover.in
+srun -l -D Slurm-Wasimoff-Container-Setup/CloverLeaf_Serial cp -f InputDecks/clover_bm16.in clover.in
 srun -l -D Slurm-Wasimoff-Container-Setup/CloverLeaf_Serial ./clover_leaf
---
+++ jobs/job_6.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+srun -l -D Slurm-Wasimoff-Container-Setup/prototype/wasi-apps/travelling_salesman ./tsp rand 14
