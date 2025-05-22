fe9748a6b1ed5d7302d55d0dcfac6ef792f3ffd8
nickw96 <nick.weiler@st.ovgu.de>
2025-05-22 23:16:51 +0200

    Add hostnames to job output

--- jobs/job_5.sh
+++ jobs/job_5.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # srun -N1 -l -D ./Slurm-Wasimoff-Container-Setup/CloverLeaf_Serial cp InputDecks/clover.in clover.in
+srun hostname
 srun -N1 date +'%Y-%m-%d %H:%M:%S'
 srun -l -D Slurm-Wasimoff-Container-Setup/CloverLeaf_Serial cp -f InputDecks/clover_bm8.in clover.in
 srun -l -D Slurm-Wasimoff-Container-Setup/CloverLeaf_Serial ./clover_leaf
--- jobs/job_9.sh
+++ jobs/job_9.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # srun -N1 -l -D ./Slurm-Wasimoff-Container-Setup/Proxels/ ./proxels -dt 0.5 -endtime 50000
+srun hostname
 srun -N1 date +'%Y-%m-%d %H:%M:%S'
 srun -l -D Slurm-Wasimoff-Container-Setup/Proxels/ ./proxels -dt 0.5 -endtime 100000
 srun -N1 date +'%Y-%m-%d %H:%M:%S'
