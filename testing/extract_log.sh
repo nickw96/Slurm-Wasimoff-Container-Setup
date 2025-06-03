addd4d39fdba12562e90d580eda70b029aaa489e
nickw96 <nick.weiler@st.ovgu.de>
2025-06-03 15:50:18 +0200

    Make extract_log.sh more user-friendly

--- testing/extract_log.sh
+++ testing/extract_log.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# needs to be executed with 'sudo'
 START=$(sed -e '1!d' $1)
 STOP=$(sed -e '2!d' $1)
-journalctl -u $2 --since "$START" --until "$STOP" >> $3
+sudo journalctl -u $2 --since "$START" --until "$STOP" >> $3
