4296b311e60cf823ee7bbda172b6e81baff8c297
nickw96 <nick.weiler@st.ovgu.de>
2025-06-06 18:37:39 +0200

    Fix transforming script

--- testing/transform_iso8601_precise_to_journalctl_readable.py
+++ testing/transform_iso8601_precise_to_journalctl_readable.py
@@ -9,14 +9,16 @@ def main():
     except IndexError:
         raise SystemExit(f"Usage: {sys.argv[0]} <txt file with timestamps>")
     
-    with open(sys.argv[1], 'r', encoding='utf-8', newline='') as file:
-        tmp = file.readline()
+    file_path = os.path.abspath(sys.argv[1])
+    
+    with open(file_path, 'r', encoding='utf-8', newline='') as file:
+        tmp = file.readline().strip()
         start = datetime.fromisoformat(tmp[:26] + tmp[29:])
-        tmp = file.readline()
+        tmp = file.readline().strip()
         end = datetime.fromisoformat(tmp[:26] + tmp[29:])
 
 
-    with open(sys.argv[1].split('.') + '_adapted.txt', 'w', encoding='utf-8', newline='\n') as output:
+    with open(file_path.split('.')[0] + '_adapted.txt', 'w', encoding='utf-8', newline='\n') as output:
         output.write(f'{start.strftime(TIMESTAMP_READ_FORMAT)}\n')
         output.write(f'{(end + timedelta(seconds=1)).strftime(TIMESTAMP_READ_FORMAT)}\n')
 
