2d29b0c75ba14cf85f359cc18bdc7eaea1759047
nickw96 <nick.weiler@st.ovgu.de>
2025-06-14 17:17:18 +0200

    Fix wasi sdk full version

--- wasi-sdk/install_wasi_sdk.sh
+++ wasi-sdk/install_wasi_sdk.sh
@@ -2,6 +2,7 @@
 export WASI_OS=linux
 export WASI_ARCH=x86_64
 export WASI_VERSION=25
-export WASI_VERSION_FULL=${WASI_VERSION}
+export WASI_VERSION_FULL=${WASI_VERSION}.0
 wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${WASI_VERSION}/wasi-sdk-${WASI_VERSION_FULL}-${WASI_ARCH}-${WASI_OS}.tar.gz
+wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-25/wasi-sdk-25.0-x86_64-linux.tar.gz
 tar xvf wasi-sdk-${WASI_VERSION_FULL}-${WASI_ARCH}-${WASI_OS}.tar.gz
