07c451fcac2a998e6ec7337b492db2dfa5826604
nickw96 <nick.weiler@st.ovgu.de>
2025-06-19 23:00:04 +0200

    Add timestamps to epilog and prolog

--- slurm-resources/epilog.sh
+++ slurm-resources/epilog.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "$(date -Ins)" >> /media/server/epilog_$(hostname)_$($SLURM_JOBID).txt
 # get node number
 host=$(hostname | sed -e 's/com//')
 # avoid problems with history expansion character
@@ -6,3 +7,4 @@ delete='!d'
 # check, if there is a suspended job on this node
 job=$(squeue -O statecompact,nodelist | sed -e "/^S /$delete")
 python3 /etc/slurm/check_alloc_node.py $host $job
+echo "$(date -Ins)" >> /media/server/epilog_$(hostname)_$($SLURM_JOBID).txt
--- slurm-resources/prolog.sh
+++ slurm-resources/prolog.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo "$(date -Ins)" >> /media/server/prolog_$(hostname)_$($SLURM_JOBID).txt
 systemctl stop wasimoff_provider.service
 systemctl disable wasimoff_provider.service
+echo "$(date -Ins)" >> /media/server/prolog_$(hostname)_$($SLURM_JOBID).txt
