0cbe108a4b240a226c8e0ca957b5e2c3e446f22a
nickw96 <nick.weiler@st.ovgu.de>
2025-06-20 00:39:30 +0200

    Fix prolog and epilog script timestamping

--- slurm-resources/epilog.sh
+++ slurm-resources/epilog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "$(date -Ins)" >> /media/server/epilog_$(hostname)_$($SLURM_JOBID).txt
+echo "$(date -Ins)" >> /home/$(echo $SLURM_JOB_USER)/epilog_$(hostname)_$(echo $SLURM_JOBID).txt
 # get node number
 host=$(hostname | sed -e 's/com//')
 # avoid problems with history expansion character
@@ -7,4 +7,4 @@ delete='!d'
 # check, if there is a suspended job on this node
 job=$(squeue -O statecompact,nodelist | sed -e "/^S /$delete")
 python3 /etc/slurm/check_alloc_node.py $host $job
-echo "$(date -Ins)" >> /media/server/epilog_$(hostname)_$($SLURM_JOBID).txt
+echo "$(date -Ins)" >> /home/$(echo $SLURM_JOB_USER)/epilog_$(hostname)_$(echo $SLURM_JOBID).txt
--- slurm-resources/prolog.sh
+++ slurm-resources/prolog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "$(date -Ins)" >> /media/server/prolog_$(hostname)_$($SLURM_JOBID).txt
+echo "$(date -Ins)" >> /home/$(echo $SLURM_JOB_USER)/prolog_$(hostname)_$(echo $SLURM_JOBID).txt
 systemctl stop wasimoff_provider.service
 systemctl disable wasimoff_provider.service
-echo "$(date -Ins)" >> /media/server/prolog_$(hostname)_$($SLURM_JOBID).txt
+echo "$(date -Ins)" >> /home/$(echo $SLURM_JOB_USER)/prolog_$(hostname)_$(echo $SLURM_JOBID).txt
