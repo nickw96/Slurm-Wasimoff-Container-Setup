54438d8aba74540541b587d9df6dcbc5302be506
nickw96 <nick.weiler@st.ovgu.de>
2025-07-16 22:06:47 +0200

    Fix epilog.sh starting provider if and only if there are no running nor suspended jobs on node

--- slurm-resources/epilog.sh
+++ slurm-resources/epilog.sh
@@ -5,6 +5,6 @@ host=$(hostname | sed -e 's/com//')
 # avoid problems with history expansion character
 delete='!d'
 # check, if there is a suspended job on this node
-job=$(squeue -O statecompact,nodelist | sed -e "/^S /$delete")
+job=$(squeue -O statecompact,nodelist | sed -e "/^S \|^R /$delete")
 python3 /etc/slurm/check_alloc_node.py $host $job
 echo "$(date -Ins)" >> /home/$(echo $SLURM_JOB_USER)/epilog_$(hostname)_$(echo $SLURM_JOBID).txt
