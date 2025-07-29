9a3203265db15ef9f5438be7d5c4fe01e74c7a82
nickw96 <nick.weiler@st.ovgu.de>
2025-07-30 01:41:52 +0200

    Update job example

--- jobs/job_example.sh
+++ jobs/job_example.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+srun -l date -Ins
+srun -l hostname
+# execute pwd with one node and log output
 srun -N1 -l pwd
+# switch to directory from option -D, execute pwd with on node and log output
 srun -N1 -l -D ./Slurm-Wasimoff-Container-Setup pwd
+srun -l date -Ins
