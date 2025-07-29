3c0d18cf1f87139b5b0365b953e10022053b5da2
nickw96 <nick.weiler@st.ovgu.de>
2025-07-30 01:46:59 +0200

    Fix job example comment

--- jobs/job_example.sh
+++ jobs/job_example.sh
@@ -3,6 +3,6 @@ srun -l date -Ins
 srun -l hostname
 # execute pwd with one node and log output
 srun -N1 -l pwd
-# switch to directory from option -D, execute pwd with on node and log output
+# switch to directory from option -D, execute pwd with one node and log output
 srun -N1 -l -D ./Slurm-Wasimoff-Container-Setup pwd
 srun -l date -Ins
